a0fc3cf2d6cee8c773dc1a7aa0391d098096f4da
zobeltran <rnzbeltran@gmail.com>
2018-01-10 17:15:44 +0800

    Added Documentations

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,8 +1,24 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * Controller for Pages MVC
+ * 
+ * This is the Controller for Pages where it connects
+ * the Page_model Models to Pages View.
+ * 
+ * @package aimex-v2/application/controllers
+ * @author Renzo Beltran <renzobeltran@outlook.com>
+ */
 class Pages extends CI_Controller {
 
+	/**
+	 * A function to call the index page
+	 * 
+	 * This function accepts $page where its defaulted to login
+	 * 
+	 * @param string $page defaulted to login
+	 */
 	public function index($page = 'login') {
 		if(!file_exists(APPPATH.'views/pages/'.$page.'.php')) {
 			show_404();
@@ -16,6 +32,13 @@ class Pages extends CI_Controller {
 		$this->load->view('_partials/_footer');
 	}
 
+	/**
+	 * A function to call views for Pages view
+	 * 
+	 * This function call view pages which is $page is defaulted to index.
+	 * 
+	 * @param string $page defaulted to index
+	 */
 	public function view($page = 'index') {
 		if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
 			show_404();
@@ -29,40 +52,81 @@ class Pages extends CI_Controller {
 		$this->load->view('_partials/_footer');
 	}
 	
+	/**
+	 * A private function for hashing password
+	 *
+	 * This functions hashes the password to Bcrypt for security
+	 * 
+	 * @param string $password do not hash
+	 * @return boolean password_hash($password, PASSWORD_BCRYPT)
+	 */	
 	private function hash_password($password){
 		return password_hash($password, PASSWORD_BCRYPT);
 	}
 
+	/**
+	 * A funtcion that validates login
+	 * 
+	 * This function validates the user if the input are with valid 
+	 * information and matches the username and password data and calls can_login
+	 * function from pages_model and creates a user session else it returns to the
+	 * login page with out a session id and throws error flashdata messages. 
+	 *
+	 * @return void
+	 */
 	function login_validation() {
 		$this->load->library('form_validation');
 		$this->form_validation->set_rules('username', 'Username', 'trim|required');
 		$this->form_validation->set_rules('password', 'Password', 'trim|required');
+		
 		if($this->form_validation->run()) {
 			$username = $this->input->post('username');
 			$password = $this->input->post('password');
-
 			$this->load->model('page_model');
-			if($this->page_model->can_login($username, $password)) {
-				$session_data = array(
-					'username' => $username //,
-					//'employee_position' => $employee_position
-				);
+			$user_id = $this->page_model->can_login($username, $password);
+			if($user_id) {
+				$session_data = array('username' => $username);
 				$this->session->set_userdata($session_data);
-				redirect(base_url().'loans/home');
+				foreach($user_id as $user_id) {
+					if($user_id->flag_deleted == 't') {
+						$this->session->set_flashdata('error', 'User has been deactivated');
+						$this->logout();
+					} else if($user_id->employee_position == 'Loans Processor') {
+						redirect(base_url().'loans/home');
+					} else if($user_id->employee_position == 'Loans Collector'){
+						redirect(base_url().'collection/home');
+					} 
+				}
 			} else {
 				$this->session->set_flashdata('error', 'Invalid Username and Password');
-				redirect(base_url());
+				$this->index();
 			}
 		} else {
 			$this->index();
 		}	
 	}
-
+	/**
+	 * A function that logs out a user
+	 * 
+	 * This function logs out the the user by unset the userdata session
+	 * and prevents unwanted access to pages beyond login and register.
+	 * This function redirects the user being logged out back to the index page
+	 *
+	 * @return void
+	 */
 	function logout() {
 		$this->session->unset_userdata('username');
-		redirect(base_url());
+		$this->index();	
 	}
-	
+	/**
+	 * A function that handles registration validation
+	 * 
+	 * This function handles the validations for registratin
+	 * and transfers the data collected to the Page Models and
+	 * redirects back to the index page.
+	 *
+	 * @return void
+	 */
 	function register_validation() {
 		$this->load->library('form_validation');
 		$this->form_validation->set_rules('employee_firstname', 'First Name', 'trim|max_length[50]|required');
@@ -81,7 +145,7 @@ class Pages extends CI_Controller {
 			);
 			$this->load->model('page_model');
 			$result = $this->page_model->register_employee($data);
-			redirect(base_url());
+			$this->index();
 		} else {
 			$this->view('register');
 		}
