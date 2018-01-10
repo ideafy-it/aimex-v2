08c4b0d55257c8148309e4eeb82ac529a50832b7
zobeltran <rnzbeltran@gmail.com>
2018-01-10 17:14:36 +0800

    Fixed Landing pages for User Positions

---
+++ application/views/collection/home.php
@@ -0,0 +1,2 @@
+<h2>Loans Collection Page</h2>
+<a href="<?php echo base_url(); ?>pages/logout">Logout</a>
--- application/views/loans/home.php
+++ application/views/loans/home.php
@@ -1,1 +1,2 @@
+<h2>Loans Processor Page</h2>
 <a href="<?php echo base_url(); ?>pages/logout">Logout</a>
