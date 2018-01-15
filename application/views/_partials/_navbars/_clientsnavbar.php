1d9adb3e7fa33b658192fc85b5a8cb2463d3b681
zobeltran <rnzbeltran@gmail.com>
2018-01-15 15:31:03 +0800

    Added Navbars

---
+++ application/views/_partials/_navbars/_clientsnavbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+    <a class="navbar-brand" href="<?php echo base_url(); ?>clients/home">Aimex</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link"><?php echo $username; ?></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url(); ?>pages/logout">Logout</a>
+            </li>
+        </ul>
+    </div>
+</nav>
