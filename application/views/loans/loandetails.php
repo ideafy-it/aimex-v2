8c21a950323ef208825eba8831851310ef70314e
zobeltran <rnzbeltran@gmail.com>
2018-05-12 18:52:31 +0800

    Fixed Data

--- application/views/loans/loandetails.php
+++ application/views/loans/loandetails.php
@@ -28,10 +28,10 @@
 		<div class="card-body row">
 			<div class="col-6">
 				<p class="card-text">Loan Amount </p>
-				<p class="card-text">Interest <?php echo $row->interest ;?></p>
-				<p class="card-text">Unearned Interest <?php echo $row->interest * $row->loanTerms ;?> </p>
-				<p class="card-text">Service Charge <?php echo $row->serviceFee ;?> </p>
-				<p class="card-text">Notarial Fee <?php echo $row->notarialFee ;?> </p>
+				<p class="card-text">Interest <?php echo $row->interest ;?> &nbsp;
+				Unearned Interest <?php echo $row->interest * $row->loanTerms ;?> &nbsp;
+				Service Charge <?php echo $row->serviceFee ;?> &nbsp;
+				Notarial Fee <?php echo $row->notarialFee ;?> </p>
 			</div>
 			 <div class="col-6">
 				<p class="card-text"><?php echo $row->monthlyPayment * $row->loanTerms ;?></p>
