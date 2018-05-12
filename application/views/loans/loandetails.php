8bf4c66879229ea53f811c499cc91b4f0ca55e41
zobeltran <rnzbeltran@gmail.com>
2018-05-12 18:34:16 +0800

    Fixed Pages

--- application/views/loans/loandetails.php
+++ application/views/loans/loandetails.php
@@ -1,10 +1,10 @@
 <div class="container">
-<?php
-    if(isset($loan_data)) {
-        if($loan_data->num_rows() > 0) {
-            foreach ($loan_data->result() as $row) {
-?>
-<div class="card">
+	<?php
+	    if(isset($loan_data)) {
+	        if($loan_data->num_rows() > 0) {
+	            foreach ($loan_data->result() as $row) {
+	?>
+	<div class="card">
             <div class="card-header">
                 <div class="row">
                     <span class="col-7"><h5><?php echo $row->clientFirstName ." ". $row->clientLastName; ?></h5></span>
@@ -21,15 +21,19 @@
                 </div>
             </div>
         </div>
-      <br><br>
-	<div>
-		<div class="col-6">
-			<p class="card-text">Loan Amount </p>
-		</div>
-		 <div class="col-6">
-			<p class="card-text"><?php echo $row->monthlyPayment * $row->loanTerms ;?></p>
-		</div>
 
+      <br><br>
+	
+	<div>
+		<div class="card-body row">
+			<div class="col-6">
+				<p class="card-text">Loan Amount </p>
+			</div>
+			 <div class="col-6">
+				<p class="card-text"><?php echo $row->monthlyPayment * $row->loanTerms ;?></p>
+			</div>
+		</div>
 	</div>
 	<?php } ?>
+
 </div>
